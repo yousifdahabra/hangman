a7ad825619a203963209067ef669d7c48ca7bc41
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-10-28 11:48:27 +0200

    Edit: customize  code base on code review, delete sizeOfArray function not needed after changing the structure of Answers array, delete console.log, and rename replace_char function name

--- scripts/game.js
+++ scripts/game.js
@@ -1,28 +1,23 @@
-var Answers = {
-  0: { hint: "A four-wheeled vehicle", answer: "car" },
-  1: { hint: "Flies high in the sky", answer: "plane" },
-  2: { hint: "King of the jungle", answer: "lion" },
-  3: { hint: "A place to keep money", answer: "bank" },
-  4: { hint: "A tropical fruit with yellow skin", answer: "banana" },
-  5: { hint: "The opposite of light", answer: "dark" },
-  6: { hint: "A hot beverage", answer: "coffee" },
-  7: { hint: "A popular pet that purrs", answer: "cat" },
-  8: { hint: "Frozen water", answer: "ice" },
-  9: { hint: "A large body of salt water", answer: "ocean" },
-  10: { hint: "You need this to breathe", answer: "air" },
-  11: { hint: "A very tall building", answer: "skyscraper" },
-  12: { hint: "A tool to tell time", answer: "clock" },
-  13: { hint: "A tasty Italian food", answer: "pizza" },
-  14: {
+var Answers = [
+  { hint: "A four-wheeled vehicle", answer: "car" },
+  { hint: "Flies high in the sky", answer: "plane" },
+  { hint: "King of the jungle", answer: "lion" },
+  { hint: "A place to keep money", answer: "bank" },
+  { hint: "A tropical fruit with yellow skin", answer: "banana" },
+  { hint: "The opposite of light", answer: "dark" },
+  { hint: "A hot beverage", answer: "coffee" },
+  { hint: "A popular pet that purrs", answer: "cat" },
+  { hint: "Frozen water", answer: "ice" },
+  { hint: "A large body of salt water", answer: "ocean" },
+  { hint: "You need this to breathe", answer: "air" },
+  { hint: "A very tall building", answer: "skyscraper" },
+  { hint: "A tool to tell time", answer: "clock" },
+  { hint: "A tasty Italian food", answer: "pizza" },
+  {
     hint: "Grows on trees and can be red, green, or yellow",
     answer: "apple",
   },
-  15: { hint: "Man’s best friend", answer: "dog" },
-  16: { hint: "Used to unlock doors", answer: "key" },
-  17: { hint: "A musical instrument with keys", answer: "piano" },
-  18: { hint: "A place to sleep at night", answer: "bed" },
-  19: { hint: "Something you wear on your feet", answer: "shoes" },
-};
+];
 var hang = {
   0: "head",
   1: "body",
@@ -31,26 +26,18 @@ var hang = {
   4: "left-leg",
   5: "right-leg",
 };
-function sizeOfArray(array) {
-  let size = 0;
-  for (let key in array) {
-    if (array.hasOwnProperty(key)) {
-      size++;
-    }
-  }
-  return size;
-}
 
 //replace a character at a particular index from geeksforgeeks
-function replaceChar(origString, replaceChar, index) {
+function replace_char(origString, replace_char, index) {
   let firstPart = origString.substr(0, index);
   let lastPart = origString.substr(index + 1);
 
-  let newString = firstPart + replaceChar + lastPart;
+  let newString = firstPart + replace_char + lastPart;
   return newString;
 }
+
 document.getElementById('answer-section').insertAdjacentHTML('beforebegin', '<div class="answer-section" id="hint-section"></div>');
-var randm_answer = Math.floor(Math.random() * sizeOfArray(Answers));
+var randm_answer = Math.floor(Math.random() * Answers.length);
 var hint_answer = Answers[randm_answer];
 var answer = hint_answer.answer.toUpperCase();
 var hint = hint_answer.hint;
@@ -69,18 +56,13 @@ divs.forEach((div) => {
     var real_live_answer = document.getElementById("answer-section");
     var letter = event.target.innerHTML;
     var index_char = answer.indexOf(letter);
-    console.log("real_live_answer", real_live_answer.innerHTML);
-    console.log("letter", letter);
-    console.log("index_char", index_char);
-    console.log("try_count", try_count);
-    console.log("answer", answer);
     if (index_char >= 0) {
-      real_live_answer.innerHTML = replaceChar(
+      real_live_answer.innerHTML = replace_char(
         real_live_answer.innerHTML,
         letter,
         index_char
       );
-      answer = replaceChar(answer, "-", index_char);
+      answer = replace_char(answer, "-", index_char);
       if (real_live_answer.innerHTML == hint_answer.answer.toUpperCase()) {
         alert("success");
         letters.style.visibility = "hidden";
@@ -93,6 +75,7 @@ divs.forEach((div) => {
     }
 
     if (try_count >= 6) {
+      alert(hint_answer.answer.toUpperCase())
       letters.style.visibility = "hidden";
     }
   });
